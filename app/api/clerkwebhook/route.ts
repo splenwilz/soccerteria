5254b924092377cdb2bb0561cb9ab4ac729da783
splenwilz <godswill.williamsz@gmail.com>
2024-05-25 11:56:27 +0100

    Fix: issues with deployment

--- app/api/clerkwebhook/route.ts
+++ app/api/clerkwebhook/route.ts
@@ -64,56 +64,13 @@ export async function POST(req: Request) {
     const eventType = evt.type
 
     if (eventType === 'user.created') {
-        const { id, email_addresses, first_name, last_name, image_url, created_at, gender } = evt.data
-
+        const { id, email_addresses, first_name, last_name, image_url, created_at } = evt.data
+        console.log(evt.data)
         if (!id || !email_addresses) {
             return new Response('Error occurred -- missing data', {
                 status: 400
             })
         }
-
-        // {
-        //     "data": {
-        //       "birthday": "",
-        //       "created_at": 1654012591514,
-        //       "email_addresses": [
-        //         {
-        //           "email_address": "example@example.org",
-        //           "id": "idn_29w83yL7CwVlJXylYLxcslromF1",
-        //           "linked_to": [],
-        //           "object": "email_address",
-        //           "verification": {
-        //             "status": "verified",
-        //             "strategy": "ticket"
-        //           }
-        //         }
-        //       ],
-        //       "external_accounts": [],
-        //       "external_id": "567772",
-        //       "first_name": "Example",
-        //       "gender": "",
-        //       "id": "user_29w83sxmDNGwOuEthce5gg56FcC",
-        //       "image_url": "https://img.clerk.com/xxxxxx",
-        //       "last_name": "Example",
-        //       "last_sign_in_at": 1654012591514,
-        //       "object": "user",
-        //       "password_enabled": true,
-        //       "phone_numbers": [],
-        //       "primary_email_address_id": "idn_29w83yL7CwVlJXylYLxcslromF1",
-        //       "primary_phone_number_id": null,
-        //       "primary_web3_wallet_id": null,
-        //       "private_metadata": {},
-        //       "profile_image_url": "https://www.gravatar.com/avatar?d=mp",
-        //       "public_metadata": {},
-        //       "two_factor_enabled": false,
-        //       "unsafe_metadata": {},
-        //       "updated_at": 1654012591835,
-        //       "username": null,
-        //       "web3_wallets": []
-        //     },
-        //     "object": "event",
-        //     "type": "user.created"
-        //   }
 
         const user = {
             clerkUserId: id,
@@ -121,7 +78,7 @@ export async function POST(req: Request) {
             ...(first_name ? { firstName: first_name } : {}),
             ...(last_name ? { lastName: last_name } : {}),
             ...(image_url ? { imageUrl: image_url } : {}),
-            ...(gender ? { gender } : {}),
+            gender: '',
             address: '',
             role: 'user',
             ...(created_at ? { createdAt: created_at } : {})
